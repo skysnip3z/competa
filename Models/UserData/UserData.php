88e1986d7d9c879caa65dd968e7d9419246c185a
skysnip3z <jcu8710@gmail.com>
2020-01-09 14:51:45 +0000

    Rubric System

--- Models/UserData/UserData.php
+++ Models/UserData/UserData.php
@@ -7,6 +7,10 @@ class UserData
 
     public function __construct($dbRow)
     {
+        if(array_key_exists("user_id", $dbRow))
+        {
+
+        }
         $this->_user_id = $dbRow['user_id'];
         $this->_username = $dbRow['username'];
         $this->_email= $dbRow['email'];
